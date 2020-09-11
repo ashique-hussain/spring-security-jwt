d5e92b3198d2f3048305c4602c6e3fe85213a222
Ashique Hussain <ashique9694@gmail.com>
2020-09-11 15:32:22 +0530

    Spring boot security jwt

---
+++ src/main/java/com/global/springsecurityjwt/HelloResource.java
@@ -0,0 +1,16 @@
+package com.global.springsecurityjwt;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello")
+@RestController
+public class HelloResource {
+
+    @GetMapping
+    public String hello(){
+        return "Hello World";
+    }
+}
---
+++ src/main/java/com/global/springsecurityjwt/controllers/AuthenticateController.java
@@ -0,0 +1,51 @@
+package com.global.springsecurityjwt.controllers;
+
+import com.global.springsecurityjwt.models.AuthenticationRequest;
+import com.global.springsecurityjwt.models.AuthenticationResponse;
+import com.global.springsecurityjwt.services.MyUserDetailsService;
+import com.global.springsecurityjwt.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/authenticate")
+@RestController
+public class AuthenticateController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws
+            Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),
+                            authenticationRequest.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+}
